ee4c7981a655841b2aaafc73bea693323e344e4c
Nahuel Paiva <nahuel.paiva@petroprix.com>
2022-10-18 00:48:54 -0300

    🎨 Improving structure

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">Initial Commit - Hi!</div>;
-}
-
-export default App;
---
+++ src/App/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Initial Commit - Hi!</div>
+    </>
+  );
+};
+
+export default App;
